ab13a885ac692c58b452f66d6cb19f01e0e3f4d8
Anna <92742853+aamnesia@users.noreply.github.com>
2021-11-25 22:19:07 +0300

    Free capacity method changed

--- L7/cargo_wagon.rb
+++ L7/cargo_wagon.rb
@@ -3,13 +3,13 @@ class CargoWagon < Wagon
   attr_reader :volume, :occupied_volume, :volume_left, :type_name
   def initialize(number, capacity, type = :cargo)
     @type_name = "Товарный"
-    @max_volume = @volume_left = capacity
+    @max_volume = capacity
     @occupied_volume = 0
     super
   end
 
   def occupy_volume(volume)
     @occupied_volume += volume
-    @volume_left -= volume
+    @volume_left = @max_volume - @occupied_volume 
   end
 end
--- L7/pas_wagon.rb
+++ L7/pas_wagon.rb
@@ -3,13 +3,13 @@ class PassengerWagon < Wagon
   attr_reader :number_of_seats, :taken_seats, :free_seats, :type_name
   def initialize(number, capacity, type = :passenger)
     @type_name = "Пассажирский"
-    @number_of_seats = @free_seats = capacity
+    @number_of_seats = capacity
     @taken_seats = 0
     super
   end
 
   def take_seat
     @taken_seats += 1
-    @free_seats -= 1
+    @free_seats = @number_of_seats - @taken_seats
   end
 end
